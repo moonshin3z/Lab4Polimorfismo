50751331fd55e9ccfa7fa4687014ea4de594e80a
edven07 <ven25155@uvg.edu.gt>
2025-11-03 21:31:05 -0600

    Update Administrador.java

--- Administrador.java
+++ Administrador.java
@@ -1,5 +1,27 @@
 public class Administrador extends Usuario{
   private String claveAdmin;
 
-  
+  public Administrador(String username, String nombre, String claveAdmin){
+    super(username, nombre, RolUsuario.ADMIN);
+    this.claveAdmin = claveAdmin;
+
+  }
+
+  public String getClaveAdmin(){
+    return claveAdmin;
+  }
+
+  public void setClaveAdmin(String claveAdmin){
+    this.claveAdmin = claveAdmin;
+  }
+
+  @Override
+    public String toString() {
+        return "Administrador{" +
+                "username='" + username + '\'' +
+                ", nombre='" + nombre + '\'' +
+                ", rol=" + rol +
+                ", claveAdmin='" + claveAdmin + '\'' +
+                '}';
 }
+
