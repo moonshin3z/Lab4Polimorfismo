e4854512945b9f98a832647438954cf8d43a4c3c
edven07 <ven25155@uvg.edu.gt>
2025-11-03 21:36:13 -0600

    Update Editor.java

--- Editor.java
+++ Editor.java
@@ -1,4 +1,28 @@
 public class Editor extends Usuario{
   private String seccion;
 
+
+  public Editor (String username, String nombre, String seccion){
+    super(username, nombre, RolUsuario.EDITOR);
+    this.seccion = seccion;
+  }
+
+  public String getSeccion(){
+    return seccion;
+  }
+
+  public void setSeccion (String seccion){
+    this.seccion = seccion;
+  }
+
+  @Override
+    public String toString() {
+        return "Editor{" +
+                "username='" + username + '\'' +
+                ", nombre='" + nombre + '\'' +
+                ", rol=" + rol +
+                ", seccion='" + seccion + '\'' +
+                '}';
+    }
 }
+
