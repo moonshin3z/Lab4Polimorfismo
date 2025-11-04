e0753a9160ed74331adf4e075dad66d6253713ff
edven07 <ven25155@uvg.edu.gt>
2025-11-03 21:23:56 -0600

    Update Usuario.java

--- Usuario.java
+++ Usuario.java
@@ -1,9 +1,47 @@
-public class Usuario{
-  private String username;
-  private String nombre;
-  private RolUsuario rol;
+public abstract class Usuario{
+  protected String username;
+  protected String nombre;
+  protected RolUsuario rol;
 
-  
+  Public Usuario(String username, String nombre, RolUsuario rol){
+      this.username = username;
+      this.nombre = nombre;
+      this.rol = rol;
+  }
+
+  Public String getUsername{
+      return username;
+  }
+
+  Public void SetUsername(String username){
+      this.username = username;
+  }
+
+  Public String getNombre{
+      return nombre;
+  }
+
+  Public void SetNombre(String nombre){
+      this.nombre = nombre;
+  }
+
+  Public RolUsuario getRol{
+      return rol;
+  }
+
+  Public void SetRol(RolUsuario rol){
+      this.rol = rol;
+  }
+
+  @Override
+  Public String toString(){
+      return "Usuario{" +
+        "username='" + username + '\'' +
+                ", nombre='" + nombre + '\'' +
+                ", rol=" + rol +
+                '}';
+  }
 }
+
 
 
