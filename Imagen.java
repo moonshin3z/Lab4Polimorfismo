de490b662795ceb5101bba6f686abf05fd5f7f89
edven07 <ven25155@uvg.edu.gt>
2025-11-03 22:00:15 -0600

    Update Imagen.java

--- Imagen.java
+++ Imagen.java
@@ -2,6 +2,57 @@ public class Imagen extends Contenido{
   private String rutaArchivo;
   private int altoPx;
   private int anchoPx;
-  private
+
+  public Imagen(int id, String titulo, String autor, String rutaArchivo, int altoPx, int anchoPx){
+    super(id, titulo, autor);
+    this.rutaArchivo = rutaArchivo;
+    this.altoPx = altoPx;
+    this.anchoPx = anchoPx;
+
+  }
+
+  public String getRutaArchivo(){
+    return rutaArchivo;
+  }
+
+  public void setRutaArchivo(String rutaArchivo){
+    this.rutaArchivo = rutaArchivo;
+  }
+
+  public int getAltoPx(){
+    return altoPx;
+  }
+
+  public void setAltoPx(int altoPx){
+    this.altoPx = altoPx;
+  }
+
+  public int getAnchoPx(){
+    return anchoPx;
+  }
+
+  public void setAnchoPx(int anchoPx){
+    this.anchoPx = anchoPx;
+  }
+
+  @Override
+    public String visualizar() {
+        return "Imagen: " + getTitulo() +
+               " (" + anchoPx + "x" + altoPx + " px) - Ruta: " + rutaArchivo;
+    }
+
+   @Override
+    public String toString() {
+        return "Imagen{" +
+                "id=" + getId() +
+                ", titulo='" + getTitulo() + '\'' +
+                ", autor='" + getAutor() + '\'' +
+                ", estado=" + getEstado() +
+                ", rutaArchivo='" + rutaArchivo + '\'' +
+                ", altoPx=" + altoPx +
+                ", anchoPx=" + anchoPx +
+                '}';
+    }
 
 }
+
