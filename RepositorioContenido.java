22973ac23a43a5422e34620b2af35d32b04a8e02
Iván Roblero <133731383+moonshin3z@users.noreply.github.com>
2025-11-03 21:57:44 -0600

    Add methods to RepositorioContenido interface

--- RepositorioContenido.java
+++ RepositorioContenido.java
@@ -1,3 +1,10 @@
 import java.util.List;
 public interface RepositorioContenido.java {
+    void agregar(Contenido c);
+    void actualizar(Contenido c);
+    void eliminar(int id);
+    Contenido buscarPorId(int id);
+    List<Contenido> listar();
 }
+  
+
