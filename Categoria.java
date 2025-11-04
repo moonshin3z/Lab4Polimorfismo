1e90f4a242ac99c592d853e3fe83d505224944ca
Quique Bran <93490937+quiqueb06@users.noreply.github.com>
2025-11-03 21:28:59 -0600

    Refactor Categoria class and add methods

--- Categoria.java
+++ Categoria.java
@@ -1,5 +1,26 @@
+import java.util.Objects;
+
 public class Categoria {
   private String nombre;
 
   public Categoria(String nombre) {
-    this.nombre = java.util.Objects.requireNonNull(nombre);
+    this.nombre = nombre;
+  }
+  public String getNombre() {
+    return nombre ;
+
+  }
+
+  public void setNombre(String nombre) {
+    this.nombre = nombre;
+
+  }
+
+  @Override
+  public String toString() {
+    return nombre;
+
+  }
+
+}
+
